c56ae37668152be917580f15d08c7c64eb2fda83
zhujun98 <zhujun981661@gmail.com>
2018-12-12 17:06:36 +0100

    Add gTest

---
+++ tests/test_trajectory.cpp
@@ -0,0 +1,17 @@
+//
+// Created by jun on 12/12/18.
+//
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+
+TEST(TestBareLinkedList, GeneralTest) {
+  ASSERT_TRUE(true);
+}
+
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+
+  return RUN_ALL_TESTS();
+}
